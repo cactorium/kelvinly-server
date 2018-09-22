b380e96205abfba854501044a66e8e12a4b4f1ce
Kelvin Ly <kelvin.ly1618@gmail.com>
2018-09-22 08:38:51 -0400

    Add setup script

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo add-apt-repository ppa:gophers/archive
+sudo apt-get update
+sudo apt-get install golang-1.10-go
+
+mkdir -p ~/go
+
+cat "export PATH=$PATH:/usr/lib/go-1.10/bin" >> ~/.bashrc
+cat "export GOPATH=~/go" >> ~/.bashrc
+
+source ~/.bashrc
+go get -u gopkg.in/russross/blackfriday.v2
+go get -u https://github.com/shurcooL/github_flavored_markdown
